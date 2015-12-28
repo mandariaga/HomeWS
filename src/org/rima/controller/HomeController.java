c123f9e04f745edc1b8ef038b840e22e6c0048db
mandariaga <mandariaga@gmail.com>
2015-12-28 17:33:39 -0300

    Se agrega método Post

--- src/org/rima/controller/HomeController.java
+++ src/org/rima/controller/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -22,6 +23,12 @@ public class HomeController {
 
 		return new ResponseEntity<String>(name + "_hola", HttpStatus.OK);
 
+	}
+	@RequestMapping(value="/shop/create", method=RequestMethod.POST, produces="application/json", consumes="application/json")
+	public ResponseEntity<Void> createUser(@RequestBody String shop){
+		System.out.println("Creating Shop "+shop);
+		
+		return new ResponseEntity<Void>( HttpStatus.OK );
 	}
 
 }
